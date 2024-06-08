b3d4df7775e32125063b1a63f5c033cc9b9d28e6
Kis-Simon David <kissimond423@gmail.com>
2024-06-08 17:31:44 +0200

    Created Secret files, DB population
    Model, Controller, factory, migration, seeders, database population implementation.

---
+++ app/Models/Secret.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Secret extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'hash',
+        'secretText',
+        'createdAt',
+        'expiresAt',
+        'remainingViews',
+    ];
+
+    // Get the datetime in the 'Y-m-d\TH:i:s.v\Z' format
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d\TH:i:s.v\Z');
+    }
+
+    // Mutator to store the datetime in the correct format
+    public function setCreatedAtAttribute($value)
+    {
+        $this->attributes['createdAt'] = Carbon::parse($value)->format('Y-m-d H:i:s.v');
+    }
+}
---
+++ database/factories/SecretFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Secret>
+ */
+class SecretFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'hash' => bin2hex(random_bytes(16)),
+            'secretText' => fake()->sentence(),
+            'expiresAt' => Carbon::now()->addMinutes(rand(1,1000))->format('Y-m-d H:i:s.v'),
+            'remainingViews' => rand(1, 10),
+        ];
+    }
+}
