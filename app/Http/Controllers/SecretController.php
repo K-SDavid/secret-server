a4e922ce7a43d3c31436960c21a6342ea7d10f74
Kis-Simon David <kissimond423@gmail.com>
2024-06-09 04:19:11 +0200

    Implemented the details function for secrets

--- app/Http/Controllers/SecretController.php
+++ app/Http/Controllers/SecretController.php
@@ -48,8 +48,8 @@ class SecretController extends Controller
             'expireAfter' => (int)$request['expireAfter'],
         ];
 
-        //Current time + the 'expireAfter' parameter in the 'Y-m-d\TH:i:s.v\Z' format
-        $expiresAt = Carbon::now()->addMinutes($data['expireAfter'])->format('Y-m-d\TH:i:s.v\Z');
+        //Current time + the 'expireAfter' minutes => the expiry date
+        $expiresAt = Carbon::now('Europe/Budapest')->addMinutes($data['expireAfter'])->format('Y-m-d H:i:s.v');
         //New variable to collect the neccessary fields for the object creation/DB insertion
         $fields = [
             'hash' => bin2hex(random_bytes(16)),
@@ -57,8 +57,29 @@ class SecretController extends Controller
             'expiresAt' => $expiresAt,
             'remainingViews' => $request['expireAfterViews'],
         ];
-        //Object creation and DB insertion
+        //Object creation and DB insertion, furthermore the response message and status code
         Secret::create($fields);
-        return $responseFormat->format(['message' => 'Successfully added a secret!'], 200);
+        return $responseFormat->format(['message' => 'Successfully added a secret!'], 201);
+    }
+
+    public function details(Secret $secret, Request $request) {
+        //Determining the response format (currently xml or json)
+        $responseFormat = $this->getResponseFormat($request);
+        
+        //Checking if the current secret is viewable (views still remaining and it is not past the expiry date)
+        $remainingViews = $secret->remainingViews;
+        if($remainingViews <= 0){
+            return $responseFormat->format(['error' => 'The chosen secret can not be viewed anymore!'], 403);
+        }
+
+        $expiresAt = Carbon::parse($secret->expiresAt)->format('Y-m-d H:i:s.v');
+        if($expiresAt <= Carbon::now('Europe/Budapest')) {
+            return $responseFormat->format(['error' => 'The chosen secret has expired!'], 403);
+        }
+        
+        //Decreasing the reamining views by 1
+        $secret->update(['remainingViews' => $remainingViews-1]);
+        //Returning the secret either as an xml or a json response with the '200' status code
+        return $responseFormat->format($secret->toArray(), 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,5 @@ use App\Http\Controllers\SecretController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::group(['middleware' => ['api']], function () {
 Route::post('/secret', [SecretController::class, 'store']);
-// });
+Route::get('/secret/{secret}', [SecretController::class, 'details']);
